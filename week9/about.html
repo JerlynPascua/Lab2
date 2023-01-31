1936f377c165a4c18c89fbca984c389f6d0e9330
Jerlyn Camille Pascua <jppascua@student.apc.edu.ph>
2023-01-31 20:14:33 +0800

    NEW UPDATES FOR PHP

---
+++ week9/about.html
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <title>Playlist Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-tale-seo-agency.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Pre-Header Area Start ***** -->
+  <div class="pre-header" id="top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-9">
+          <div class="left-info">
+            <ul>
+              <li><a href="#"><i class="fa fa-phone"></i>+63 912 345 6789</a></li>
+              <li><a href="#"><i class="fa fa-envelope"></i>jppascua@student.apc.edu.ph</a></li>
+              <li><a href="#"><i class="fa fa-map-marker"></i>Asia Pacific College - Magallanes, Makati</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-3">
+          <div class="social-icons">
+            <ul>
+              <li><a href="https://www.facebook.com/cumsyy" target="_blank"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/jerlyn-camille-pascua-3ab4b818b/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+              <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Pre-Header Area End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png" alt="" style="max-width: 112px;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li><a href="index.html">Home</a></li>
+                      <li><a href="index.html">Services</a></li>
+                      <li><a href="index.html">Projects</a></li>
+                      <li class="has-sub">
+                          <a href="javascript:void(0)">Pages</a>
+                          <ul class="sub-menu">
+                              <li><a href="about.html">Playlist</a></li>
+                              <li><a href="resources.html">More</a></li>
+                          </ul>
+                      </li>
+                      <li><a href="index.html">Infos</a></li>
+                      <li><a href="index.html">Contact</a></li>
+                  </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 align-self-center">
+          <div class="caption  header-text">
+            <h6>Jerlyn Camille Pascua</h6>
+			<h6>BSIT MI 211</h6>
+            <div class="line-dec"></div>
+            <h4>Here's my <em>Music Playlist</em></h4>
+            <p>Take note these are all embeds, please make sure your version of chrome, edge, etc. is up to date.</p>
+          </div>
+        </div>
+        <div class="col-lg-5 align-self-center">
+          <img src="assets/images/about-us-image.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="video-info section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="video-thumb">
+            <img src="assets/images/video-thumb.jpg" alt="">
+            <a href="https://www.youtube.com/watch?v=L81sF1AsMKA" target="_blank"><i class="fa fa-play"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading">
+            <h2>This is a youtube video link of the songs I usually listen to.</h2>
+            <div class="line-dec"></div>
+            <p>The youtube video contains K-Pop songs that lets me relax or vibe through dull times.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="happy-clients section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading">
+            <h2>Different feels of a <em>Spotify Playlist</em></h2>
+            <div class="line-dec"></div>
+            <p>note that this is a spotify embed and this is purely previews of songs. for full versions of the songs please be located to <a href="https://open.spotify.com/" target="_blank">Spotify!</a></p>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="naccs">
+            <div class="tabs">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="menu">
+                    <div class="active"><span>Studying Playlist</span></div>
+                    <div><span>Studying Playlist 2</span></div>
+                    <div><span>Personal Playlist </span></div>
+                    <div class="last-item"><span>Personal Playlist 2</span></div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <ul class="nacc">
+                    <li class="active">
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Study Session Playlist</h4>
+                            <div class="line-dec"></div>
+							<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/02mnn2ZI8mmD1e3IvdVG9x?utm_source=generator" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" style="margin-left:auto; margin-right:auto;"></iframe>
+                            </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Study Session Playlist 2</h4>
+                            <div class="line-dec"></div>
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4sfBDAsUYCIEKn1lm7oqDh?utm_source=generator" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Personal Playlist</h4>
+                            <div class="line-dec"></div>
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7Kr9LORBPv00dIfitgisA8?utm_source=generator" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="row">
+                          <div class="col-lg-7">
+                            <h4>Personal Playlist 2</h4>
+                            <div class="line-dec"></div>
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1OCEbAQoq9x6xKBfxbePKM?utm_source=generator" width="100%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                          </div>
+                          <div class="col-lg-5 align-self-center">
+                            <img src="assets/images/happyclient-01.jpg" alt="">
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="cta section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <h4>Suggestions for website design ?</h4>
+        </div>
+        <div class="col-lg-4">
+          <div class="main-button">
+            <a href="#">Contact Me Now!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-12">
+       <p><br>This webpage will serve as an example portfolio for JCPP's Webprog completion</p>
+		
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
---
+++ week9/index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <title>Jerlyn's Portfolio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-tale-seo-agency.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Pre-Header Area Start ***** -->
+  <div class="pre-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-9">
+          <div class="left-info">
+            <ul>
+              <li><a href="#"><i class="fa fa-phone"></i>+63 912 345 6789</a></li>
+              <li><a href="#"><i class="fa fa-envelope"></i>jppascua@student.apc.edu.ph</a></li>
+              <li><a href="#"><i class="fa fa-map-marker"></i>Asia Pacific College - Magallanes, Makati</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-3">
+          <div class="social-icons">
+            <ul>
+              <li><a href="https://www.facebook.com/cumsyy" target="_blank"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/jerlyn-camille-pascua-3ab4b818b/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+              <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Pre-Header Area End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo.png" alt="" style="max-width: 112px;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                      <li class="scroll-to-section"><a href="#services">Services</a></li>
+                      <li class="scroll-to-section"><a href="#projects">Projects</a></li>
+                      <li class="has-sub">
+                          <a href="javascript:void(0)">Pages</a>
+                          <ul class="sub-menu">
+                              <li><a href="about.html">Playlist</a></li>
+                              <li><a href="resources.html">More</a></li>
+                          </ul>
+                      </li>
+                      <li class="scroll-to-section"><a href="#infos">Infos</a></li>
+                      <li class="scroll-to-section"><a href="#contact">Contact</a></li>
+                  </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="main-banner" id="top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7">
+          <div class="caption header-text">
+            <h6>Jerlyn Camille Pascua</h6>
+			<h6>BSIT MI 211</h6>
+            <div class="line-dec"></div>
+            <h4>My <em>Example Portfolio</em><br>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧<span>WEBPROG</span></h4>
+            <p>This is a modified template website with 3 HTML pages in completion to my subject course WEBPROG,to view more be directed to - <a href="index.html">Home</a>, <a href="about.html">About</a>, and <a href="faqs.html">FAQ</a>.</p>
+            <div class="main-button scroll-to-section"><a href="#services">Discover More</a></div>
+            <span>or</span>
+            <div class="second-button"><a href="faqs.html">Check our FAQs</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="services section" id="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-6">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="section-heading">
+                <h2>Here are some <em>Basic Information</em> &amp;
+                  <span>My Thoughts</span> on this website</h2>
+                  <div class="line-dec"></div>
+                  <p>please note that I am making the best out of this test portfolio</p>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="assets/images/services-01.jpg" alt="" class="templatemo-feature">
+                </div>
+                <h4>I go to school at Asia Pacific College</h4>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="assets/images/services-02.jpg" alt="" class="templatemo-feature">
+                </div>
+                <h4>Asia Pacific College is related to National University</h4>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="assets/images/services-03.jpg" alt="" class="templatemo-feature">
+                </div>
+                <h4>I wish to learn more about website making</h4>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="assets/images/services-04.jpg" alt="" class="templatemo-feature">
+                </div>
+                <h4>I hope to build connections of information through website</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="projects section" id="projects">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h2>My <em>Works</em> &amp; <span>Projects</span></h2>
+            <div class="line-dec"></div>
+            <p>If you are redirected on the top of the page that will indicate that there is no link attached. It will serve as a future project</p>
+          </div>
+        </div>
+      </div> 
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="owl-features owl-carousel">
+            <div class="item">
+              <img src="assets/images/projects-01.jpg" alt="">
+              <div class="down-content">
+                <h4>open slot for future projects</h4>
+                <a href="#"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <div class="item">
+              <img src="assets/images/projects-02.jpg" alt="">
+              <div class="down-content">
+                <h4>open slot for future projects</h4>
+                <a href="#"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <div class="item">
+              <img src="assets/images/projects-03.jpg" alt="">
+              <div class="down-content">
+                <h4>open slot for future projects</h4>
+                <a href="#"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <div class="item">
+              <img src="assets/images/projects-04.jpg" alt="">
+              <div class="down-content">
+                <h4>open slot for future projects</h4>
+                <a href="#"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <div class="item">
+              <img src="assets/images/projects-02.jpg" alt="">
+              <div class="down-content">
+                <h4>open slot for future projects</h4>
+                <a href="#"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <div class="item">
+              <img src="assets/images/projects-03.jpg" alt="">
+              <div class="down-content">
+                <h4>open slot for future projects</h4>
+                <a href="#"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="infos section" id="infos">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="main-content">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="left-image">
+                  <img src="assets/images/left-infos.jpg" alt="">
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="section-heading">
+                  <h2>Total progress of the <em>Website</em> &amp; My <span>Projects</span></h2>
+                  <div class="line-dec"></div>
+                  <p>The progress bars may change within time as soon as I finish my future projects and works</p>
+                </div>
+                <div class="skills">
+                  <div class="skill-slide marketing">
+                    <div class="fill"></div>
+                    <h6>Website's Frontend code</h6>
+                    <span>90%</span>
+                  </div>
+                  <div class="skill-slide digital">
+                    <div class="fill"></div>
+                    <h6>Website's Backend code</h6>
+                    <span>70%</span>
+                  </div>
+                  <div class="skill-slide media">
+                    <div class="fill"></div>
+                    <h6>Projects listing</h6>
+                    <span>95%</span>
+                  </div>
+                </div>
+                <p class="more-info">Be informed that these statistics may change in time.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="contact-us section" id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="contact-us-content">
+            <div class="row">
+              <div class="col-lg-4">
+                <div id="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.1900774536175!2d121.01912631582154!3d14.531117582669072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c93018658925%3A0x6de8e16ad051b59e!2sAsia%20Pacific%20College!5e0!3m2!1sen!2sph!4v1672363232448!5m2!1sen!2sph" width="100%" height="670px" frameborder="0" style="border:0; border-radius: 23px;" allowfullscreen=""></iframe>
+				</div>
+              </div>
+              <div class="col-lg-8">
+                <form id="contact-form" action="" method="post">
+                  <div class="row">
+                    <div class="col-lg-12">
+                      <div class="section-heading">
+                        <h2><em>Contact Me</em> &amp; Get In <span>Touch</span></h2>
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <fieldset>
+                        <input type="name" name="name" id="name" placeholder="Your Name..." autocomplete="on" required>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-6">
+                      <fieldset>
+                        <input type="surname" name="surname" id="surname" placeholder="Your Surname..." autocomplete="on" required>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-6">
+                      <fieldset>
+                        <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your E-mail..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-6">
+                      <fieldset>
+                        <input type="subject" name="subject" id="subject" placeholder="Subject..." autocomplete="on" >
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <textarea name="message" id="message" placeholder="Your Message"></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-lg-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="orange-button">Send Message Now</button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+                <div class="more-info">
+                  <div class="row">
+                    <div class="col-lg-4">
+                      <div class="info-item">
+                        <i class="fa fa-phone"></i>
+                        <h4><a href="#">123-4567</a></h4>
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div class="info-item">
+                        <i class="fa fa-envelope"></i>
+                        <h4><a href="#">jppascua@student.apc.edu.ph</a></h4>
+                        <h4><a href="#">camseey@gmail.com</a></h4>
+                      </div>
+                    </div>
+                    <div class="col-lg-4">
+                      <div class="info-item">
+                        <i class="fa fa-map-marker"></i>
+                        <h4><a href="#">Makati, Magallanes - Asia Pacific College</a></h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-12">
+		<p><br>This webpage will serve as an example portfolio for JCPP's Webprog completion</p>
+		
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+
+  </body>
+
+</html>
